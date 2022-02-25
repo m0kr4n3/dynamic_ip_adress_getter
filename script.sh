d1e5608aa09db3512cf8525a8dfabd6bb19e6d70
m0kr4n3 <m0kr4n3@m0kr4n3.m0kr4n3>
2022-02-25 18:40:22 -0500

    updating the script

--- script.sh
+++ script.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-/usr/bin/curl --silent ipinfo.io/ip > /tmp/.newone.txt
-publicIP=$(/usr/bin/cat /tmp/.newone.txt)
+if [[ $# -eq 0 ]]; then
+        echo -e "[-] Usage : $0 <the private git repository to push your public ip>\n ex : $0  ~/RPI_address"
+        exit
+fi;
+current_dir="$PWD"
+dir="$1"
 
-if [[ ! -e publicIP.txt ]]; then
-	/usr/bin/echo 0 > /root/RPiAdress/.publicIP.txt;
+
+# Making the path absolute
+if [[ ! "${dir:0:1}" == "/" ]]; then
+        dir="$PWD/$dir";
+fi;
+
+# Testing if the directory is a real git repository
+if [[ ! -d "$dir/.git" ]]; then
+        echo -e "no such git repository : $1/.git"
+        exit
+fi;
+
+# Fetching the public ip address
+publicIP=$(/usr/bin/curl --silent ipinfo.io/ip)
+
+#initiating the current ip address to 0 if it doesn't exist, in order to write on it
+if [[ ! -e "$dir/publicIP.txt" ]]; then
+	/usr/bin/echo 0 > "$dir/publicIP.txt";
 fi
 
-ancientIP=$(cat /root/RPiAdress/.publicIP.txt);
+ancientIP=$(cat "$dir/publicIP.txt");
 
 if [[ "$publicIP" != "$ancientIP" ]] ; then
-	/usr/bin/echo $publicIP > /root/RPiAdress/.publicIP.txt ;
-	commitName=$(/usr/bin/date|/usr/bin/cut -d" " -f5);
-	cd /root/RPiAdress/;
+	/usr/bin/echo $publicIP > "$dir/publicIP.txt" ;
+	commitName=$(/usr/bin/date|/usr/bin/cut -d" " -f4);
+	cd "$dir";
 	/usr/bin/git add . > /dev/null 2>&1;
 	/usr/bin/git commit -m $commitName  > /dev/null 2>&1;
-	/usr
+	/usr/bin/git push origin main > /dev/null 2>&1;
+	cd "$current_dir";
+	echo "Public ip address is successfully uploaded !"
+fi;
+
